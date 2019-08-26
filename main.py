ce3a5696321a15b1812c3b76c7a51fcf3e5fd335
ftamon <yuu.tmn123@gmail.com>
2019-08-27 01:16:36 +0900

    リファクタリングと楓さんセリフ取得

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ import csv
 import urllib3
 import certifi
 from bs4 import BeautifulSoup
-
 import time
 
-for i in range(1, 15, 1):
+base_url = 'https://icws.indigo-bell.com/search?page='
+# idol_page = '&q=%E3%81%AB%E3%81%AA&st=n' # 仁奈ちゃん
+idol_page = '&q=高垣&st=n'
+out_file = 'imas_cg_kaede.txt'
+
+for i in range(1, 16, 1):
     time.sleep(3)
-    print('page : ', i)
-    target_url = 'https://icws.indigo-bell.com/search?page=' + str(i) + '&q=%E3%81%AB%E3%81%AA&st=n'
-    # httpsの証明書検証を実行している
+    print(f'page : {i}')
+    target_url = base_url + str(i) + idol_page
+    # httpsの証明書検証を実行
     http = urllib3.PoolManager(
         cert_reqs='CERT_REQUIRED',
         ca_certs=certifi.where())
@@ -20,9 +24,9 @@ for i in range(1, 15, 1):
     soup = BeautifulSoup(r.data, 'html.parser')
 
     tbody = soup.findAll('tbody')[0]
-    rows = tbody.findAll("tr")
+    rows = tbody.findAll('tr')
 
-    with open("imas_cg_nina.txt", "a", encoding='shift-jis') as f:
+    with open(out_file, 'a', encoding='shift-jis') as f:
         writer = csv.writer(f)
         for row in rows:
             csv_row = []
